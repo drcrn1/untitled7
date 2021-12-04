7abf2d46bd64088b49a0cdebaffdebd44c0e2c28
drcrn1 <79824542+drcrn1@users.noreply.github.com>
2021-12-05 03:11:08 +0900

    Update lab8.java

--- src/lab8.java
+++ src/lab8.java
@@ -1,14 +1,1 @@
-public class lab8 {
-    public static void main(String[] args) {
-        People people =  new People("Anton","Danilov",17);
-        people.walk();
-        people.run();
-        people.voice();
-        Student student = new Student("Anton2","Danilov2",19);
-        student.walk();
-        student.learn();
-        Teacher teacher = new Teacher("Anton3","Danilov3",36);
-        teacher.voice();
-        teacher.teach();
-    }
-}
+
